220a0b86e036612b643f9d187ac5ff073be670ee
frustrated-learner <professionalplayer50@gmail.com>
2021-12-14 08:23:04 +0600

    Problem 1 Solution

---
+++ Arrays/1--Sort_Odd_numbers_while_Keeping_Even_numbers_at_original_position.py
@@ -0,0 +1,26 @@
+# You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+# Examples :
+# [7, 1]  =>  [1, 7]
+# [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+def sort_array(source_array):
+    source_arr_copy = source_array
+    odd_arr = []
+    i = 0
+    
+    for ints in source_array:
+        if ints % 2 != 0:
+            odd_arr.append(ints)
+
+    odd_arr.sort()
+
+    for index, all_ints in enumerate(source_arr_copy):
+        if all_ints % 2 != 0:
+            source_arr_copy[index] = odd_arr[i]
+            i += 1
+
+    return source_arr_copy
+
+print(sort_array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
