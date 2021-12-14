f80fe40611237c0317e9e074e74d9b2bd0b19f5d
frustrated-learner <professionalplayer50@gmail.com>
2021-12-14 20:09:11 +0600

    Problem 2 Solution

---
+++ Arrays/2--Check_if_you_have_won_the_Lottery_Jackpot.py
@@ -0,0 +1,28 @@
+# Time to win the lottery!
+
+# Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+
+# Example ticket:
+# [ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+
+# To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, you get a mini win. Note you can only have one mini win per sub array.
+
+# Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+
+# All inputs will be in the correct format. Strings on tickets are not always the same length.
+
+def bingo(ticket,win):
+    mini_wins = 0
+
+    for row in ticket:
+        for letters in row[0]:
+            if ord(letters) == row[1]:
+                mini_wins += 1
+                break
+
+    if mini_wins >= win:
+        return "Winner!"
+    else:
+        return "Loser!"
+
+print(bingo([ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ], 1))
