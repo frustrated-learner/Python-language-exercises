56588ec8f12640b369b7191d63bbd1d74d7f1312
frustrated-learner <professionalplayer50@gmail.com>
2022-01-20 20:09:36 +0600

    Problem 1 Solution

---
+++ Date & Time/1--Genrate_Human_Readable_Time.py
@@ -0,0 +1,15 @@
+# Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+# HH = hours, padded to 2 digits, range: 00 - 99
+# MM = minutes, padded to 2 digits, range: 00 - 59
+# SS = seconds, padded to 2 digits, range: 00 - 59
+# The maximum time never exceeds 359999 (99:59:59)
+
+def make_readable(seconds):
+    sec = seconds % (24 * 3600)
+    hour = seconds // 3600
+    sec %= 3600
+    min = sec // 60
+    sec %= 60
+
+    return "%02d:%02d:%02d" % (hour, min, sec)
